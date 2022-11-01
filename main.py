71eb2227c0e05cd0e70e4a8b0aac2f3276daaefe
APLiu <75256935+quantumxiaol@users.noreply.github.com>
2022-11-01 13:38:35 +0800

    Digit Recogni
    数据集:
    训练集：42000张图片，每张图片的高度为 28 像素，宽度为 28 像素，总共 784像素。这个像素值是一个介于 0 和 255 之间的整数，包括 0 和 255。每个像素都有一个与之关联的单个像素值，表示该像素的明暗，数字越大表示越暗。
    任务：
    对于测试集中的 28000 张图像中的每一张，输出其标签。

---
+++ main.py
@@ -0,0 +1,75 @@
+
+import torch
+import torch.nn as nn
+import pandas as pd
+
+class DatasetMNIST(torch.utils.data.Dataset):
+    def __init__(self,data):
+        self.data = data
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self,index):
+        item = self.data.iloc[index]
+        label,features = item[0],torch.Tensor(item[1:])
+        return features,label
+
+def main():
+    input_size = 784 #28*28
+    num_classes = 10
+    num_epochs = 5
+    batch_size = 30
+    learning_rate = 0.001
+    data_train = pd.read_csv('./data/train.csv')
+    data_test = pd.read_csv('./data/test.csv')
+    train_dataset = DatasetMNIST(data_train)
+    test_dataset = DatasetMNIST(data_test)
+
+    train_loader = torch.utils.data.DataLoader(dataset=train_dataset,batch_size=batch_size,shuffle=True)
+    test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
+
+    model = nn.Linear(input_size,num_classes)
+
+    criterion = nn.CrossEntropyLoss()
+
+    optimizer = torch.optim.SGD(model.parameters(),lr=learning_rate)
+
+    total_step = len(train_loader)
+
+    for epoch in range(num_epochs):
+        for i ,(features,labels) in enumerate(train_loader):
+            outputs =model(features)
+            loss = criterion(outputs,labels)
+
+
+            optimizer.zero_grad()
+
+            loss.backward()
+
+            optimizer.step()
+
+            if(i+1) % 10 == 0 :
+                print("Rpoch[{}/{}],Step[{}/{}],Loss:{:.4f}".format(epoch+1 , num_epochs , i+1 , total_step , loss.item()))
+
+        with torch.no_grad():
+            correct = 0
+            total = 0
+            for (features , labels) in test_loader:
+
+                outputs = model(features)
+
+                _ , predicted = torch.max(outputs.data , 1)
+
+
+                total += labels.size(0)
+
+                correct += (predicted == labels).sum()
+            print("Accuracy of model on the 10000 test images:{}%".format(100*correct/total))
+
+        total.save(model.state_dict() , "model.ckpt")
+
+if __name__ =='__main__':
+    main()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
